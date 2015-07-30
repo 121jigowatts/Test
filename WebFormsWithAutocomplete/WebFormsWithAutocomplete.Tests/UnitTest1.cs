b5d3303ea5511d398fd9eff44b2bd0897aa77578
adihsoy <sh.yoshida@live.jp>
2015-07-31 01:43:09 +0900

    Initial

---
+++ WebFormsWithAutocomplete/WebFormsWithAutocomplete.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebFormsWithAutocomplete.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
